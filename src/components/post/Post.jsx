61f8ebec2fc63ae7347359d0a9e1004b142f0cdc
Ayush Bhattarai <ayushbhattaraiofficial@gmail.com>
2024-04-29 09:51:33 +0545

    Day 2 of 2: frontend portion done

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./post.scss";
 import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
 import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import TextsmsRoundedIcon from "@mui/icons-material/TextsmsRounded";
+import ShareRoundedIcon from "@mui/icons-material/ShareRounded";
 import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 import { Link } from "react-router-dom";
+import { Comments } from "../comments/Comments";
 export const Post = ({ post }) => {
+    const [commentOpen, setCommentOpen] = useState(false);
+    // TEMPORARY
+    const liked = true;
     return (
         <div className="post">
             <div className="container">
@@ -27,8 +32,32 @@ export const Post = ({ post }) => {
                     </div>
                     <MoreHorizRoundedIcon />
                 </div>
-                <div className="content"></div>
-                <div className="info"></div>
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className="info">
+                    <div className="item">
+                        {liked ? (
+                            <FavoriteRoundedIcon />
+                        ) : (
+                            <FavoriteBorderRoundedIcon />
+                        )}
+                        12 Likes
+                    </div>
+                    <div
+                        className="item"
+                        onClick={() => setCommentOpen(!commentOpen)}
+                    >
+                        <TextsmsRoundedIcon />
+                        12 Comments
+                    </div>
+                    <div className="item">
+                        <ShareRoundedIcon />
+                        12 Shares
+                    </div>
+                </div>
+                {commentOpen && <Comments />}
             </div>
         </div>
     );
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,71 @@
 import React from "react";
 import "./profile.scss";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import XIcon from "@mui/icons-material/X";
+import PlaceRoundedIcon from "@mui/icons-material/PlaceRounded";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import MoreVertRoundedIcon from "@mui/icons-material/MoreVertRounded";
+import { Posts } from "../../components/posts/Posts";
 
 export const Profile = () => {
-    return <div className="profile">Profile</div>;
+    return (
+        <div className="profile">
+            <div className="images">
+                <img
+                    src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    className="cover"
+                />
+                <img
+                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    alt=""
+                    className="profilePic"
+                />
+            </div>
+            <div className="profileContainer">
+                <div className="uInfo">
+                    <div className="left">
+                        <a href="http://facebook.com">
+                            <FacebookRoundedIcon fontSize="large" />
+                        </a>
+                        <a href="http://linkedin.com">
+                            <LinkedInIcon fontSize="large" />
+                        </a>
+                        <a href="http://instagram.com">
+                            <InstagramIcon fontSize="large" />
+                        </a>
+                        <a href="http://pinterest.com">
+                            <PinterestIcon fontSize="large" />
+                        </a>
+                        <a href="http://twitter.com">
+                            <XIcon fontSize="large" />
+                        </a>
+                    </div>
+                    <div className="center">
+                        <span>Jack Doe</span>
+                        <div className="info">
+                            <div className="item">
+                                <PlaceRoundedIcon />
+                                <span>Brazil</span>
+                            </div>
+                            <div className="item">
+                                <LanguageRoundedIcon />
+                                <span>jack.com</span>
+                            </div>
+                        </div>
+                        <button>follow</button>
+                    </div>
+                    <div className="right">
+                        <EmailRoundedIcon />
+                        <MoreVertRoundedIcon />
+                    </div>
+                </div>
+                <Posts />
+            </div>
+        </div>
+    );
 };
